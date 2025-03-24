ea3a2781356c45388241fe76c09a0b553173ace3
Oraz Giyoev <49120405+oraz4@users.noreply.github.com>
2025-03-24 16:38:00 -0400

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("7828769985:AAGADDv0_xy0otFLzJhusUjpYrLMvDxwTXI")
 DATABASE_URL = os.getenv("DATABASE_URL")
